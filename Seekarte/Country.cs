097d9f07afca0f64f2f730c66bad4a4a1e095256
Jorrit Stroh <jorrit.stroh@outlook.de>
2020-01-08 22:01:27 +0100

    inital commit

---
+++ Seekarte/Country.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows.Forms;
+
+namespace Seekarte
+{
+    struct Ship
+    {
+        enum Schiff { Schnellsegler, Transportschiff };
+        string name;
+        Location location;
+    }
+
+    struct Location
+    {
+        private int locationX;
+        private int locationY;
+    }
+    class Country
+    {
+        private string _name;
+        private string _password = "123";
+        public bool passwordSet = false;
+
+        public Country(string name)
+        {
+            this._name = name;
+        }
+
+        public void SetPassowrd(string _password)
+        {
+            if (passwordSet)
+            {
+                MessageBox.Show("Passwort bereits vergeben", "Passwor vergeben", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            }
+            else
+            {
+                this._password = _password;
+            }
+        }
+
+        public string GetPassword()
+        {
+            return _password;
+        }
+
+        Ship test123;
+    }
+}
---
+++ Seekarte/Form1.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Windows.Forms;
+
+namespace Seekarte
+{
+    public partial class Form1 : Form
+    {
+        Country polen = new Country("polen");
+
+        public Form1()
+        {
+
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            string pwdInput;
+            if (!polen.passwordSet)
+            {
+                var pwdOutput = Interaction.InputBox("Bitte legen Sie ihr Passwort fest", "Passwort", "");
+                polen.SetPassowrd(pwdOutput);
+                //MessageBox.Show("Bitte legen Sien ein Passwort fest", "Enter Password", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+            }
+            //MessageBox.Show(test, "Enter Password", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            do
+            {
+                pwdInput = Interaction.InputBox("Bitte geben Sie ihr Passwort ein", "Passwort", "");
+            } while (pwdInput != polen.GetPassword());
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
